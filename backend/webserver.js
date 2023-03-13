3f193e25ef92b64f94ed29052cb6b11ed72b5504
Denis Abrosimov <darkpers@gmail.com>
2023-03-13 22:53:12 +0300

    add: process GET reqquets

--- backend/webserver.js
+++ backend/webserver.js
@@ -1,53 +1,74 @@
 import * as http from 'http'
 
 export default class Webserver {
-    constructor(db) {
-        this.host = '0.0.0.0';
-        this.port = 3000;
-        this.db = db;
-    }
+	constructor(db) {
+		this.host = '0.0.0.0';
+		this.port = 3000;
+		this.db = db;
+	}
 
-    start() {
-        this.server = http.createServer((req, res) => {
-            let statusCode = 404;
-            let resultData = '404\n';
-            if (req.method == 'POST') {
+	start() {
+		this.server = http.createServer((req, res) => {
+			let statusCode = 404;
+			let resultData = '404\n';
+			if (req.method == 'POST') {
 
-                let body = '';
-                req.on('data', (data) => {
-                    body += data;
-                })
+				let body = '';
+				req.on('data', (data) => {
+					body += data;
+				})
 
-                req.on('end', () => {
-                    if (req.url == '/mac') {
-                        let args = JSON.parse(body);
-                        //console.log(args.assigns)
-                        if (args.assigns != null) {
-                            let result = this.db.searchByAssign(args.assigns)
-                            if (result != null) {
-                                statusCode = 200
-                                resultData = JSON.stringify(result);
-                                //console.log('ok', resultData)
-                            }
-                            res.statusCode = statusCode;
-                            res.setHeader('Content-Type', 'text/plain');
-                            res.end(resultData);
-                        }
-                    } else {
-                        res.statusCode = statusCode;
-                        res.setHeader('Content-Type', 'text/plain');
-                        res.end(resultData);
-                    }
-                })
-            } else {
-                res.statusCode = statusCode;
-                res.setHeader('Content-Type', 'text/plain');
-                res.end(resultData);
-            }
-        });
+				req.on('end', () => {
+					if (req.url == '/mac') {
+						let args = JSON.parse(body);
+						//console.log(args.assigns)
+						if (args.assigns != null) {
+							let result = this.db.searchByAssign(args.assigns)
+							if (result != null) {
+								statusCode = 200
+								resultData = JSON.stringify(result);
+								//console.log('ok', resultData)
+							}
+							res.statusCode = statusCode;
+							res.setHeader('Content-Type', 'text/plain');
+							res.end(resultData);
+						}
+					} else {
+						res.statusCode = statusCode;
+						res.setHeader('Content-Type', 'text/plain');
+						res.end(resultData);
+					}
+				})
+			} else if(req.method == 'GET') {
+				let whiteListUrls = ['/', 'index.html', 'styles.css', 'main.js'];
+				if (whiteListUrls.includes(req.url)) {
+					let fileName = req.url;
+					if (req.url == '/') 
+						fileName = '/index.html';	
+	
+					fs.readFile(__dirname + fileName, (err, data) => {
+						if (err) {
 
-        this.server.listen(this.port, this.host, () => {
-            console.log('server started');
-        })
-    }
+						} else {
+							statusCode = 200;
+						}
+					});
+
+				} else {
+					res.statusCode = statusCode;
+					res.setHeader('Content-Type', 'text/plain');
+					res.end(resultData);
+				}
+
+			} else {
+				res.statusCode = statusCode;
+				res.setHeader('Content-Type', 'text/plain');
+				res.end(resultData);
+			}
+		});
+
+		this.server.listen(this.port, this.host, () => {
+			console.log('server started');
+		})
+	}
 }
